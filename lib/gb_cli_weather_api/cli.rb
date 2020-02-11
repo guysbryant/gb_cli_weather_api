9778fab3f6d78199783e3709ea1493bb62871c69
guysbryant <guy.s.bryant@gmail.com>
2020-02-11 17:27:41 -0500

    Add menu

--- lib/gb_cli_weather_api/cli.rb
+++ lib/gb_cli_weather_api/cli.rb
@@ -2,6 +2,13 @@ class GbCliWeatherApi::CLI
 
     def initialize
         GbCliWeatherApi::APIKEY.set_api_key
-        puts GbCliWeatherApi::API.zip_code_weather
+        menu
+    end
+
+    def menu
+        puts "Let's show your weather.\n Enter your zipcode: "
+        zipcode = gets.strip.downcase
+        menu if zipcode.length != 5 || zipcode =~ /[a-z]/
+        puts GbCliWeatherApi::API.zip_code_weather(zipcode)
     end
 end
