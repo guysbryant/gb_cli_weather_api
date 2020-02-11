b55d3865b59135e290c4cf3a3df522468375d7eb
guysbryant <guy.s.bryant@gmail.com>
2020-02-11 16:51:34 -0500

    Test out httparty before creating api class

--- lib/gb_cli_weather_api/cli.rb
+++ lib/gb_cli_weather_api/cli.rb
@@ -1,5 +1,14 @@
 class GbCliWeatherApi::CLI
-    def start
-        puts "cli"
+    include HTTParty
+    base_uri "api.openweathermap.org/data/2.5/weather"
+    @@api_key
+
+    def initialize
+        GbCliWeatherApi::APIKEY.set_api_key
     end
+
+    def zip_code_weather
+        self.class.get("?zip=29611,us&appid=#{GbCliWeatherApi::APIKEY.api_key}")
+    end
+
 end
