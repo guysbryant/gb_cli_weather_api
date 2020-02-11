2b1b9e583b54684831cd4cd36706d9f70fb6c18f
guysbryant <guy.s.bryant@gmail.com>
2020-02-11 17:28:25 -0500

    Add zipcode argument to zip_code_weather method to allow dynamic weather information

--- lib/gb_cli_weather_api/api.rb
+++ lib/gb_cli_weather_api/api.rb
@@ -3,7 +3,7 @@ class GbCliWeatherApi::API
     base_uri "api.openweathermap.org/data/2.5/weather"
     @@api_key
 
-    def self.zip_code_weather
-        get("?zip=29611,us&appid=#{GbCliWeatherApi::APIKEY.api_key}")
+    def self.zip_code_weather(zipcode)
+        get("?zip=#{zipcode},us&appid=#{GbCliWeatherApi::APIKEY.api_key}")
     end
 end
