eeac586de791df4ece7ebb287ee933b05e36188e
guysbryant <guy.s.bryant@gmail.com>
2020-02-11 16:50:57 -0500

    Add environment file to manage the environment

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,11 +1,9 @@
+require 'httparty'
+require 'pry'
 require "gb_cli_weather_api/version"
 require "gb_cli_weather_api/cli"
+require "gb_cli_weather_api/api_key"
 
 module GbCliWeatherApi
   class Error < StandardError; end
-  class Temperature
-    def self.display
-      "70"
-    end
-  end
 end
