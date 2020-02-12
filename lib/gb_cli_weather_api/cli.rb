850ea8e32498734e553e71d04e2dbe8fae7e9f44
guysbryant <guy.s.bryant@gmail.com>
2020-02-12 12:23:14 -0500

    Add dynamic pattern matching to call appropriate get_weather_by method based on user input

--- lib/gb_cli_weather_api/cli.rb
+++ lib/gb_cli_weather_api/cli.rb
@@ -6,20 +6,26 @@ class GbCliWeatherApi::CLI
     end
 
     def menu
-        puts "Let's show your weather.\n Enter your zipcode: "
-        # zipcode = gets.strip.downcase
-        # menu if zipcode.length != 5 || zipcode =~ /[a-z]/
-        # get_weather_by_zip(zipcode)
-        city = gets.strip.downcase
-        get_weather_by_city(city)
-    end
-
-    def get_weather_by_city(city)
-        weather = GbCliWeatherApi::API.city_weather(city)
+        puts "Let's show your weather.\n Enter your city and state abbreviation separated by a comma, or zipcode: "
+        input = gets.strip.downcase
+        if input.match(/\A[0-9]{5}\z/)
+            weather = get_weather_by_zip(input)
+        elsif input.match(/\A[a-z]+, [a-z]{2}\z/i)
+            weather = get_weather_by_city(input)
+        else
+            puts "You entered #{input} but I can only work with a 5 digit zipcode or a city and state abbreviation separated by a comma."
+            puts "For example, 29611 is the zipcode for Greenville, Sout Carolina and I can work with that zipcode."
+            puts "I can also work with Greenville, SC."
+            menu
+        end
         binding.pry
     end
 
+    def get_weather_by_city(city)
+        GbCliWeatherApi::API.city_weather(city)
+    end
+
     def get_weather_by_zip(zipcode)
-        weather = GbCliWeatherApi::API.zip_code_weather(zipcode)
+        GbCliWeatherApi::API.zip_code_weather(zipcode)
     end
 end
