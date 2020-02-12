982390722769b7668616fae035e8d1ad227ef60e
guysbryant <guy.s.bryant@gmail.com>
2020-02-12 08:51:39 -0500

    Add default argument for zipcode

--- lib/gb_cli_weather_api/api.rb
+++ lib/gb_cli_weather_api/api.rb
@@ -1,9 +1,8 @@
 class GbCliWeatherApi::API
     include HTTParty
     base_uri "api.openweathermap.org/data/2.5/weather"
-    @@api_key
 
-    def self.zip_code_weather(zipcode)
+    def self.zip_code_weather(zipcode = 29611)
         get("?zip=#{zipcode},us&appid=#{GbCliWeatherApi::APIKEY.api_key}")
     end
 end
